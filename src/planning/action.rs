b62b406e611015e941f6d972a3743f57b8ea064c
pycogram <kaluifesinachi@yahoo.com>
2026-06-30 02:02:21 +0100

    feat: add preconditions, effects, is_applicable and apply to Action

--- src/planning/action.rs
+++ src/planning/action.rs
@@ -1,34 +1,69 @@
 use serde::{Deserialize, Serialize};
+use super::State;
 
-/// An action that can be performed
+/// An action with STRIPS-style preconditions and effects
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Action {
     name: String,
     parameters: Vec<String>,
+    preconditions: Vec<(String, String)>,
+    effects: Vec<(String, String)>,
 }
 
 impl Action {
-    /// Create a new action
     pub fn new(name: impl Into<String>) -> Self {
         Self {
             name: name.into(),
             parameters: Vec::new(),
+            preconditions: Vec::new(),
+            effects: Vec::new(),
         }
     }
 
-    /// Add a parameter
     pub fn with_parameter(mut self, param: impl Into<String>) -> Self {
         self.parameters.push(param.into());
         self
     }
 
-    /// Get the name
+    /// Add a precondition: the state variable `key` must equal `value`
+    pub fn with_precondition(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.preconditions.push((key.into(), value.into()));
+        self
+    }
+
+    /// Add an effect: set state variable `key` to `value` after execution
+    pub fn with_effect(mut self, key: impl Into<String>, value: impl Into<String>) -> Self {
+        self.effects.push((key.into(), value.into()));
+        self
+    }
+
     pub fn name(&self) -> &str {
         &self.name
     }
 
-    /// Get parameters
     pub fn parameters(&self) -> &[String] {
         &self.parameters
+    }
+
+    pub fn preconditions(&self) -> &[(String, String)] {
+        &self.preconditions
+    }
+
+    pub fn effects(&self) -> &[(String, String)] {
+        &self.effects
+    }
+
+    /// Returns true if all preconditions hold in the given state
+    pub fn is_applicable(&self, state: &State) -> bool {
+        self.preconditions.iter().all(|(k, v)| state.matches(k, v))
+    }
+
+    /// Apply this action's effects to a state, returning the resulting state
+    pub fn apply(&self, state: &State) -> State {
+        let mut next = state.clone();
+        for (k, v) in &self.effects {
+            next = next.set(k.clone(), v.clone());
+        }
+        next
     }
 }
